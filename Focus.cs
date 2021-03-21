dabe263361bab95747ceea295f53414934f4ddd9
Ashley Parker <80204602+ash-parker@users.noreply.github.com>
2021-03-21 04:39:23 +0300

    update v1.3.2.1

---
+++ Focus.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace encryptor
+{
+    public static class Focus
+    {
+        private const int Keysize = 256;
+
+        private const int DerivationIterations = 1000;
+
+        public static string Encrypt(string plainText, string passPhrase)
+        {
+            byte[] saltStringBytes = Generate256BitsOfRandomEntropy(32);
+            byte[] ivStringBytes = Generate256BitsOfRandomEntropy(16);
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            Rfc2898DeriveBytes password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations);
+            try
+            {
+                byte[] keyBytes = password.GetBytes(Keysize / 8);
+                using (RijndaelManaged symmetricKey = new RijndaelManaged())
+                {
+                    symmetricKey.BlockSize = 128;
+                    symmetricKey.Mode = CipherMode.CBC;
+                    symmetricKey.Padding = PaddingMode.PKCS7;
+                    using (ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                    {
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                            {
+                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                                cryptoStream.FlushFinalBlock();
+                                byte[] cipherTextBytes = saltStringBytes;
+                                cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+                                cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                                memoryStream.Close();
+                                cryptoStream.Close();
+                                return Convert.ToBase64String(cipherTextBytes);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return "Encrypt Error";
+            }
+        }
+
+        private static byte[] Generate256BitsOfRandomEntropy(int i)
+        {
+            byte[] randomBytes = new byte[i];
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            rngCsp.GetBytes(randomBytes);
+            return randomBytes;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace encryptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Focus::Encryptor";
+            ConsoleColor white = ConsoleColor.White;
+            ConsoleColor red = ConsoleColor.Red;
+            ConsoleColor green = ConsoleColor.Green;
+            Console.WriteLine("FocusCrypt [Version 1.3.2.1]");
+            Console.WriteLine("https://www.github.com/focuscrypt/encryptor");
+            Console.ForegroundColor = green;
+            Console.WriteLine("Women's Rights Matters.");
+            Console.WriteLine();
+            Console.ForegroundColor = white;
+        start:
+            Console.Write("Create a Password:");
+            string pass = string.Empty;
+            ConsoleKey key;
+            do
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+                if (key == ConsoleKey.Backspace && pass.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    pass = pass[0..^1];
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write("*");
+                    pass += keyInfo.KeyChar;
+                }
+            } while (key != ConsoleKey.Enter);
+            Console.WriteLine();
+            if (string.IsNullOrWhiteSpace(pass))
+            {
+                Console.ForegroundColor = red;
+                Console.WriteLine("Please set a password!");
+                Console.ForegroundColor = white;
+                Console.WriteLine();
+                goto start;
+            }
+            Console.ForegroundColor = green;
+            Console.WriteLine("You have created a password.");
+            Console.ForegroundColor = white;
+            Console.WriteLine();
+        second:
+            Console.Write("Enter a plain text:");
+            string ptext = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(ptext))
+            {
+                Console.ForegroundColor = red;
+                Console.WriteLine("Please enter any text.");
+                Console.ForegroundColor = white;
+                Console.WriteLine();
+                goto second;
+            }
+            Console.ForegroundColor = green;
+            Console.WriteLine("Ok.");
+            Console.ForegroundColor = white;
+            Console.WriteLine("The encryption process has started. Please wait...");
+            Console.WriteLine("The encryption process is complete. Result:");
+            Console.ForegroundColor = green;
+            Console.WriteLine();
+            Console.WriteLine("-----");
+            Console.ForegroundColor = white;
+            Console.WriteLine(Focus.Encrypt(ptext, pass));
+            Console.ForegroundColor = green;
+            Console.WriteLine("-----");
+            Console.WriteLine();
+            Console.ForegroundColor = white;
+            Console.WriteLine("Don't forget to copy the encrypted text!");
+            Console.Write("Do you want to do another encryption?(y/n)");
+            string c = Console.ReadLine();
+            if (c == "y")
+            {
+                Console.WriteLine();
+                goto start;
+            }
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
